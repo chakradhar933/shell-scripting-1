cd626d778ef9ffd7a187ee56ce5d07cde414d0d4
chakri <chakri1@gmail.com>
2023-07-21 16:01:51 +0530

    04

--- shell-scripts/04-variables.sh
+++ shell-scripts/04-variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # user to enter the values dynamically that is through read command.
 echo "please enter username:"
-read Username
+read -s Username
 echo "user is:${Username}"
+echo "please enter password:"
+read -s Password
+echo "Password is: ${Password}"
